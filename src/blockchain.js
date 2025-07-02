e5782766d3e31fb60b690f4da462398cd48f3821
xumengxia <1540926580@qq.com>
2025-07-02 09:47:13 +0800

    生成并打印

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,4 +1,4 @@
-// 1.迷你区块链--woniu-chain
+// 1.迷你区块链--lemon-chain
 // 2.区块链的生成，新增，校验
 // 3.交易
 // 4.非对称加密
@@ -19,3 +19,33 @@
 //     "prevHash": "0",//上一个区块的,哈希值1
 //     "nonce": "0"// 随机数
 // }]
+
+const crypto = require('crypto')
+class Blockchain {
+    constructor() {
+        this.blockchain = [];
+        this.data = []
+        this.difficulty = 4
+        const hash = this.computebHush(0, '0', new Date().getTime(), 'hello lemonChain', 1)
+        console.log(hash);
+
+    }
+    // 挖矿
+    mine() {
+        // 生成新的区块
+        // 不停地算hash,知道符合难度的条件 新增区块
+    }
+    // 生成新区块
+    generateNewBlock() {
+
+    }
+    // 计算哈希
+    computebHush(index, prevHash, timestamp, data, nonce) {
+        return crypto.createHash('sha256').update(index + prevHash + timestamp + data + nonce).digest('hex')
+    }
+    // 校验区块
+    isValidBlock() { }
+    // 校验区块链
+    isValidChain() { }
+}
+let bc = new Blockchain()
